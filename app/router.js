b9588c1bdb99a48ae56f03693ca15e97f5e9a59a
Amir Abu Hasan <amirabuhasan@dahmakan.com>
2019-06-12 01:16:49 +0800

    initial commit

--- app/router.js
+++ app/router.js
@@ -7,6 +7,18 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('home', function() {
+    this.route('mike');
+  });
+  this.route('orgs'); // /orgs
+  this.route('org', { path: 'org/:id' }, function() { // /org/e
+    this.route('repos');
+    this.route('repo', { path: ':repoid' }, function() {
+      this.route('contributors');
+      this.route('issues');
+    }); // org/jquery/jquery-ui
+  });
+  this.route('notfound', {path: '*path' });
 });
 
 export default Router;
